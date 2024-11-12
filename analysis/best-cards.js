f11ab51e5fd40fc3c792e6ed9cf5fd643903fbe3
Chase Manning <chase@manning.dev>
2024-11-12 12:34:33 +0000

    :rocket: order decks based on specific strength

--- analysis/best-cards.js
+++ analysis/best-cards.js
@@ -8,113 +8,113 @@ const POPULARITY_IMPORTANCE = 0.25;
 const DECK_NAMES = [
   {
     mainCard: "2 Pikachu ex A1 96",
-    deckName: "Pikachu ex"
+    deckName: "Pikachu ex",
   },
   {
     mainCard: "1 Pikachu ex A1 96",
-    deckName: "Pikachu ex"
+    deckName: "Pikachu ex",
   },
   {
     mainCard: "2 Mewtwo ex A1 129",
-    deckName: "Mewtwo ex"
+    deckName: "Mewtwo ex",
   },
   {
     mainCard: "1 Mewtwo ex A1 129",
-    deckName: "Mewtwo ex"
+    deckName: "Mewtwo ex",
   },
   {
     mainCard: "2 Articuno ex A1 84",
-    deckName: "Articuno ex"
+    deckName: "Articuno ex",
   },
   {
     mainCard: "1 Articuno ex A1 84",
-    deckName: "Articuno ex"
+    deckName: "Articuno ex",
   },
   {
     mainCard: "2 Greninja A1 89",
-    deckName: "Greninja"
+    deckName: "Greninja",
   },
   {
     mainCard: "1 Greninja A1 89",
-    deckName: "Greninja"
+    deckName: "Greninja",
   },
   {
     mainCard: "2 Charizard ex A1 36",
-    deckName: "Charizard ex"
+    deckName: "Charizard ex",
   },
   {
     mainCard: "1 Charizard ex A1 36",
-    deckName: "Charizard ex"
+    deckName: "Charizard ex",
   },
   {
     mainCard: "2 Exeggutor ex A1 23",
-    deckName: "Exeggutor ex"
+    deckName: "Exeggutor ex",
   },
   {
     mainCard: "1 Exeggutor ex A1 23",
-    deckName: "Exeggutor ex"
+    deckName: "Exeggutor ex",
   },
   {
     mainCard: "2 Marowak ex A1 153",
-    deckName: "Marowak ex"
+    deckName: "Marowak ex",
   },
   {
     mainCard: "1 Marowak ex A1 153",
-    deckName: "Marowak ex"
+    deckName: "Marowak ex",
   },
   {
     mainCard: "2 Lapras ex P-A 14",
-    deckName: "Lapras ex"
+    deckName: "Lapras ex",
   },
   {
     mainCard: "2 Gengar ex A1 123",
-    deckName: "Gengar ex"
+    deckName: "Gengar ex",
   },
   {
     mainCard: "1 Gengar ex A1 123",
-    deckName: "Gengar ex"
+    deckName: "Gengar ex",
   },
   {
     mainCard: "2 Dragonite A1 185",
-    deckName: "Dragonite"
+    deckName: "Dragonite",
   },
   {
     mainCard: "1 Dragonite A1 185",
-    deckName: "Dragonite"
+    deckName: "Dragonite",
   },
   {
     mainCard: "2 Weezing A1 177",
-    deckName: "Poison"
+    deckName: "Poison",
   },
   {
     mainCard: "2 Wigglytuff ex A1 195",
-    deckName: "Wigglytuff ex"
+    deckName: "Wigglytuff ex",
   },
   {
     mainCard: "1 Weezing A1 177",
-    deckName: "Poison"
+    deckName: "Poison",
   },
   {
     mainCard: "1 Wigglytuff ex A1 195",
-    deckName: "Wigglytuff ex"
+    deckName: "Wigglytuff ex",
   },
   {
     mainCard: "2 Blaine A1 221",
-    deckName: "Blaine"
+    deckName: "Blaine",
   },
   {
     mainCard: "1 Blaine A1 221",
-    deckName: "Blaine"
+    deckName: "Blaine",
   },
   {
     mainCard: "2 Machamp ex A1 146",
-    deckName: "Machamp ex"
+    deckName: "Machamp ex",
   },
   {
     mainCard: "1 Machamp ex A1 146",
-    deckName: "Machamp ex"
-  }
-]
+    deckName: "Machamp ex",
+  },
+];
 
 const getDeckName = (deck) => {
   const { cards } = deck;
@@ -124,61 +124,33 @@ const getDeckName = (deck) => {
     }
   }
   return null;
-}
+};
 
 const decksWithoutNames = JSON.parse(fs.readFileSync("decks.json"));
 
-const deckScores = decksWithoutNames.map((deck) => {
-  const name = getDeckName(deck);
-  return {
-    ...deck,
-    name,
-  };
-}).filter((deck) => deck.name);
+const deckScores = decksWithoutNames
+  .map((deck) => {
+    const name = getDeckName(deck);
+    return {
+      ...deck,
+      name,
+    };
+  })
+  .filter((deck) => deck.name);
 
-const deckClassses = [];
+const uniqueDeckNames = deckScores
+  .map((deck) => deck.name)
+  .filter((value, index, self) => self.indexOf(value) === index);
 
-for (const deck of deckScores) {
-  const existingDeck = deckClassses.find((d) => d.name === deck.name);
-  if (existingDeck) {
-    existingDeck.wins += deck.wins;
-    existingDeck.losses += deck.losses;
-    existingDeck.totalGames += deck.totalGames;
-  } else {
-    deckClassses.push({
-      name: deck.name,
-      wins: deck.wins,
-      losses: deck.losses,
-      totalGames: deck.totalGames,
-    });
-  }
-}
-
-const totalDeckGames = deckClassses.reduce(
+const totalDeckGames = deckScores.reduce(
   (acc, deck) => acc + deck.totalGames,
   0
 );
 
-const score = (deck) => {
-  const winRate = deck.wins / deck.totalGames;
-  const popularity = deck.totalGames / totalDeckGames;
-  return winRate * WINRATE_IMPORTANCE + popularity * POPULARITY_IMPORTANCE;
-};
+const bestDecks = [];
 
-const sorted = deckClassses.sort((a, b) => {
-  const aScore = score(a);
-  const bScore = score(b);
-  return bScore - aScore;
-});
-
-for (const currentDeck of sorted) {
-  const matchingGames = deckScores.filter(
-    (game) => game.name === currentDeck.name
-  );
-  console.log("");
-  console.log(
-    `=== ${currentDeck.name} (${(score(currentDeck) * 100).toFixed(0)}%) ===`
-  );
+for (const deckName of uniqueDeckNames) {
+  const matchingGames = deckScores.filter((game) => game.name === deckName);
 
   const cards = [];
 
@@ -205,16 +177,14 @@ for (const currentDeck of sorted) {
     0
   );
 
+  const cardScore = (card) => {
+    const aWinRate = card.wins / card.totalGames;
+    const aPopularity = card.totalGames / totalGames;
+    return aWinRate * WINRATE_IMPORTANCE + aPopularity * POPULARITY_IMPORTANCE;
+  };
+
   const sortedCards = cards.sort((a, b) => {
-    const aWinRate = a.wins / a.totalGames;
-    const aPopularity = a.totalGames / totalGames;
-    const aScore =
-      aWinRate * WINRATE_IMPORTANCE + aPopularity * POPULARITY_IMPORTANCE;
-    const bWinRate = b.wins / b.totalGames;
-    const bPopularity = b.totalGames / totalGames;
-    const bScore =
-      bWinRate * WINRATE_IMPORTANCE + bPopularity * POPULARITY_IMPORTANCE;
-    return bScore - aScore;
+    return cardScore(b) - cardScore(a);
   });
 
   const deck = [];
@@ -229,11 +199,28 @@ for (const currentDeck of sorted) {
     const cardName = rawCardName.join(" ");
     if (EXCLUDE.includes(cardName)) continue;
     if (EXCLUDE.includes(card.name)) continue;
-    if (deck.some((c) => c.includes(cardName))) continue;
+    if (deck.some((c) => c.name.includes(cardName))) continue;
     if (amount + cardsInDeck > 20) continue;
-    deck.push(card.name);
+    deck.push(card);
     cardsInDeck += amount;
   }
 
-  console.log(deck.join("\n"));
+  const deckScore = deck.reduce((acc, card) => acc + cardScore(card), 0);
+
+  bestDecks.push({
+    name: deckName,
+    cards: deck.map((card) => card.name),
+    score: deckScore,
+  });
+}
+
+bestDecks.sort((a, b) => b.score - a.score);
+
+const topScore = bestDecks[0].score;
+
+for (const deck of bestDecks) {
+  const percentage = (deck.score / topScore) * 100;
+  console.log("");
+  console.log(`===== ${deck.name} (${Math.round(percentage)}%) =====`);
+  console.log(deck.cards.join("\n"));
 }
