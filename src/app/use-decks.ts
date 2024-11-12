a74a3ce204747a0a0f568b35f70af260754df549
Chase Manning <chase@manning.dev>
2024-11-12 15:55:01 +0000

    :rocket: add all decks

--- src/app/use-decks.ts
+++ src/app/use-decks.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import DECKS from "./decks";
+import DECKS from "./decks.json";
 
 const CARDS_URL =
   "https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v1.json";
@@ -20,7 +20,23 @@ interface FullDeckType {
   id: string;
   name: string;
   cards: CardType[];
+  score: number;
 }
+
+const cardNameToAmount = (name: string): number => {
+  const items = name.split(" ");
+  const amount = items[0];
+  return Number(amount);
+};
+
+const cardNameToId = (name: string): string => {
+  const items = name.split(" ");
+  const id = items[items.length - 1];
+  const padded = id.padStart(3, "0");
+  const a1 = name.includes("A1");
+  const pa = name.includes("P-A");
+  return `${a1 ? "a1" : pa ? "pa" : ""}-${padded}`;
+};
 
 const useDecks = (): FullDeckType[] | null => {
   const [cards, setCards] = useState<CardType[] | null>(null);
@@ -34,14 +50,24 @@ const useDecks = (): FullDeckType[] | null => {
   if (!cards) return null;
 
   return DECKS.map((oldDeck) => {
+    const deckCards = [];
+    for (const cardName of oldDeck.cards) {
+      const amount = cardNameToAmount(cardName);
+      const id = cardNameToId(cardName);
+      const card = cards?.find((card) => card.id === id);
+      if (!card) {
+        throw new Error(`Card not found: ${id}`);
+      }
+      for (let i = 0; i < amount; i++) {
+        deckCards.push(card);
+      }
+    }
+
     const deck: FullDeckType = {
-      id: oldDeck.id,
+      id: oldDeck.name.toLowerCase().replace(/\s/g, "-"),
       name: oldDeck.name,
-      cards: oldDeck.cards.map((cardId) => {
-        const card = cards?.find((card) => card.id === cardId);
-        if (!card) throw new Error(`Card not found: ${cardId}`);
-        return card;
-      }),
+      cards: deckCards,
+      score: oldDeck.score,
     };
     return deck;
   });
