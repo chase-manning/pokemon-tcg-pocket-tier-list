558ce52f13dc7c53fafb9e502db2398b90530575
Chase Manning <chase@manning.dev>
2024-12-26 16:16:27 +0000

    :art: refactor expansion scaling logic

--- analysis/utils/get-multiplier.js
+++ analysis/utils/get-multiplier.js
@@ -11,7 +11,12 @@ const getMultiplier = (
   const datePercentage = (deckDate - oldestDate) / (newestDate - oldestDate);
   const recencyMultiplier =
     datePercentage * (newMultiplier - oldMultiplier) + oldMultiplier;
-  const isAfterExpansionMultiplier = isAfterExpansion ? 20 : 1;
+  const timeUntilExpansion = expansionReleaseDate - oldestDate;
+  const timeSinceExpansion = newestDate - expansionReleaseDate;
+  const newExpansionMultiplier = (timeUntilExpansion / timeSinceExpansion) * 4;
+  const isAfterExpansionMultiplier = isAfterExpansion
+    ? newExpansionMultiplier
+    : 1;
   return recencyMultiplier * isAfterExpansionMultiplier;
 };
 
