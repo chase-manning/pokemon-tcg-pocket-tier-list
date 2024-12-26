202c9d3c4c0165b6aca4095f91fd3132d3291e63
Chase Manning <chase@manning.dev>
2024-12-26 16:50:02 +0000

    :rocket: add debug info to deck card

--- src/components/DeckCard.tsx
+++ src/components/DeckCard.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { FullDeckType } from "../app/use-decks";
+import { DEBUG, MIN_PERCENT_TO_QUALIFY } from "../app/config";
 
-const StyledDeckCard = styled.button`
+const StyledDeckCard = styled.button<{ $disabled: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -14,6 +15,10 @@ const StyledDeckCard = styled.button`
   overflow: hidden;
   position: relative;
   cursor: pointer;
+  position: relative;
+
+  filter: ${(props) => (props.$disabled ? "grayscale(1)" : "none")};
+  opacity: ${(props) => (props.$disabled ? 0.5 : 1)};
 `;
 
 const DeckImage = styled.img`
@@ -22,6 +27,17 @@ const DeckImage = styled.img`
   left: 50%;
   transform: translateX(-50%);
   height: 280%;
+`;
+
+const Percent = styled.div`
+  font-size: 1.8rem;
+  font-weight: 700;
+  position: absolute;
+  bottom: 0.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 0.5rem 1rem;
+  background: rgba(0, 0, 0, 0.7);
 `;
 
 interface Props {
@@ -39,9 +55,22 @@ const DeckCard = ({ deck }: Props) => {
 
   mainCard = exactCard || mainCard || deck.cards[0];
 
+  const isAboveMin = deck.percentOfGames > MIN_PERCENT_TO_QUALIFY;
+  if (!isAboveMin && !DEBUG) {
+    return null;
+  }
+
+  const round = (num: number, decimals = 2) => {
+    return Math.round(num * 10 ** decimals) / 10 ** decimals;
+  };
+
   return (
-    <StyledDeckCard onClick={() => navigate(`/deck/${deck.id}`)}>
+    <StyledDeckCard
+      onClick={() => navigate(`/deck/${deck.id}`)}
+      $disabled={!isAboveMin}
+    >
       <DeckImage src={mainCard.image} alt={deck.name} />
+      {DEBUG && <Percent>{round(deck.percentOfGames, 5)}%</Percent>}
     </StyledDeckCard>
   );
 };
