1846576cdc479292104816705c64d38b90b3c64c
leonid-dalin <infoleonid@protonmail.com>
2026-08-12 04:01:08 +0300

    fix: forgot to export formatMatch from format-name utility

--- analysis/src/utils/format-name.ts
+++ analysis/src/utils/format-name.ts
@@ -1,33 +1,16 @@
 import { Card } from "./types";
 
-const formatName = (cards: Card[], match: string[]): string => {
-  return match
-    .map((cardName) => {
-      const cardNameParts = cardName.split(" ");
-      const id = cardNameParts[cardNameParts.length - 1];
-      const padded = id.padStart(3, "0");
-      const newCardNameParts = [...cardNameParts.slice(0, -1), padded];
-      const newCardName = newCardNameParts.join("-").toLowerCase();
-      return newCardName;
-    })
-    .join("&");
+const formatName = (_cards: Card[], match: string[]): string => {
+    return match
+        .map((cardName) => {
+            const cardNameParts = cardName.split(" ");
+            const id = cardNameParts.pop() || "";
+            const padded = id.padStart(3, "0");
+            return [...cardNameParts, padded].join("-").toLowerCase();
+        })
+        .join("&");
 };
+
+export const formatMatch = (match: string[]): string => formatName([], match);
 
 export default formatName;
-
-export const formatMatch = (match: string[]) => {
-  return match
-    .map((cardName) => {
-      const cardNameParts = cardName.split(" ");
-      const id = cardNameParts[cardNameParts.length - 1];
-      const setRaw = cardNameParts[cardNameParts.length - 2];
-      let set = setRaw === "P-A" ? "PA" : setRaw;
-      set = set === "P-B" ? "PB" : set;
-      const idLength = id.length;
-      const nameLength = cardName.length - idLength - setRaw.length - 2;
-      const name = cardName.slice(0, nameLength);
-      const paddedId = id.padStart(3, "0");
-      return `${name}-${set}-${paddedId}`;
-    })
-    .join("&");
-};
